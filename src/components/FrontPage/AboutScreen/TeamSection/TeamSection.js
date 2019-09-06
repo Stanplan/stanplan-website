d4d92670721e6d13ab3647f70536dd2314315dd8
rickygv99 <rickygv99@yahoo.com>
2019-09-05 20:31:00 -0700

    Rewrote code displaying team members

--- src/components/FrontPage/AboutScreen/TeamSection/TeamSection.js
+++ src/components/FrontPage/AboutScreen/TeamSection/TeamSection.js
@@ -2,12 +2,84 @@ import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import styles from './TeamSection.module.css';
+
 import ricky from 'assets/images/profiles/ricky.jpg';
 import julia from 'assets/images/profiles/julia.jpg';
 import adam from 'assets/images/profiles/adam.svg';
-import styles from './TeamSection.module.css';
+
+const ROW_LENGTH = 4;
 
 class TeamSection extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      team: [
+        {
+          name: "Ricky Grannis-Vu",
+          title: "Founder",
+          image: ricky
+        },
+        {
+          name: "Julia Truitt",
+          title: "Founder",
+          image: julia
+        },
+        {
+          name: "Adam Keppler",
+          title: "Founder",
+          image: adam
+        }
+      ]
+    };
+  }
+
+  renderCard(cellIndex) {
+    let name = this.state.team[cellIndex].name;
+    let title = this.state.team[cellIndex].title;
+    let image = this.state.team[cellIndex].image;
+    return (
+      <Col>
+        <Card>
+          <Card.Img variant="top" src={image} height='280'/>
+          <Card.Body>
+            <Card.Title>{name}</Card.Title>
+            <Card.Text>
+              {title}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  }
+
+  renderRow(rowIndex) {
+    let jsx = [];
+    for (let j = 0; j < ROW_LENGTH; j++) {
+      if (rowIndex + j < this.state.team.length) {
+        jsx.push(this.renderCard(rowIndex + j));
+      } else {
+        jsx.push(<Col></Col>);
+      }
+    }
+    return jsx;
+  }
+
+  renderCards() {
+    let jsx = [];
+    for (let i = 0; i < this.state.team.length; i += ROW_LENGTH) {
+      jsx.push(
+        <Row className={styles.cardContainer}>
+        {
+          this.renderRow(i)
+        }
+        </Row>
+      );
+    }
+    return jsx;
+  }
 
   render() {
     return (
@@ -15,41 +87,9 @@ class TeamSection extends Component {
         <h4>
           Our Team
         </h4>
-        <Row className={styles.cardContainer}>
-          <Col>
-            <Card>
-              <Card.Img variant="top" src={ricky} height='280'/>
-              <Card.Body>
-                <Card.Title>Ricky Grannis-Vu</Card.Title>
-                <Card.Text>
-                  Founder
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <Card.Img variant="top" src={adam} height='280'/>
-              <Card.Body>
-                <Card.Title>Adam Keppler</Card.Title>
-                <Card.Text>
-                  Founder
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <Card.Img variant="top" src={julia} height='280'/>
-              <Card.Body>
-                <Card.Title>Julia Truitt</Card.Title>
-                <Card.Text>
-                  Founder
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        {
+          this.renderCards()
+        }
       </div>
     );
   }
