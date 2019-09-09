2ebca9819c7b44f3ccb725f00590e8ee0c5fdfa4
rickygv99 <rickygv99@yahoo.com>
2019-09-09 15:17:42 -0700

    Added instructions and list of job openings to Jobs Section

--- src/components/FrontPage/AboutScreen/JobsSection/JobsSection.js
+++ src/components/FrontPage/AboutScreen/JobsSection/JobsSection.js
@@ -11,9 +11,18 @@ class JobsSection extends Component {
         </h4>
         <p>
           We are very excited to continue expanding StanPlan and are looking for
-          teammates who are just as excited to join our team! If you are
-          interested in joining the StanPlan team, please reach out to us at
-          <i> contact@stanplan.com</i>.
+          teammates who are just as excited as us to join our team! If you are
+          interested in joining the StanPlan team, send us an e-mail at
+          <i> contact@stanplan.com</i> with the subject line
+          `Job Application: [Your name here]`. Please attach your resume as well
+          as the position(s) you would like to be considered for. We are currently
+          looking to fill the following positions:<br/><br/>
+          <strong>Web developer</strong><br/>
+           - Palo Alto, CA<br/>
+          <strong>Full-stack developer</strong><br/>
+           - Palo Alto, CA<br/>
+          <strong>UI / UX designer</strong><br/>
+           - Palo Alto, CA
         </p>
       </div>
     );
